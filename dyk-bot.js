942f3b28550e254b3400374f240b7e855a85991a
juan0tron <juandglover@gmail.com>
2017-06-20 22:56:06 -0400

    Add client credentials to an external file that can be loaded in

--- dyk-bot.js
+++ dyk-bot.js
@@ -1,7 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login("YourKeyHere");
+// Read bot credentials from a file
+const fs = require("fs");
+let credentials = JSON.parse(fs.readFileSync('./client_key.json',"utf8"));
+
+client.login(credentials.token);
 
 client.on("ready", () => {
   console.log("I am ready!");
@@ -19,4 +23,17 @@ client.on("message", (message) => {
 
 });
 
+function loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'file.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+
+      // .open will NOT return a value but simply returns undefined in async mode so use a callback
+      callback(xobj.responseText);
+
+    }
+  }
+  xobj.send(null);
 }
