d76c08380de7d83618085669db5c03659b3f3ba3
juan0tron <juandglover@gmail.com>
2017-06-22 12:57:48 -0400

    Add feeds file so names/shows can be looked up by channel ID

---
+++ feeds.js
@@ -0,0 +1,25 @@
+{
+  "name":       "Movies with Mikey",
+  "feed_url":   "https://www.youtube.com/feeds/videos.xml?playlist_id=PLdGl5mi0XeW2iK2sVp2ni_VDRKrmfF_-Z"
+  "channel_id": "326893762049736704" // Dev channel
+},
+{
+  "name":       "PortsCenter",
+  "feed_url":   "https://www.youtube.com/feeds/videos.xml?playlist_id=PLdGl5mi0XeW2f3re8q6C7fUGjYFtQltyu",
+  "channel_id": "326893844664811520", // Dev channel
+},
+{
+  "name":       "Local58",
+  "feed_url":   "https://www.youtube.com/feeds/videos.xml?playlist_id=PLdGl5mi0XeW0rOb8Ahw4L3kGflHJ9IdqF",
+  "channel_id": "326893865565028352",
+},
+{
+  "name":       "28 Plays Later",
+  "feed_url":   "https://www.youtube.com/feeds/videos.xml?playlist_id=PLdGl5mi0XeW1rBxwI5JdcstHXHrKKeNJP",
+  "channel_id": null,
+},
+{
+  "name":       "ChainsawSuit Solves",
+  "feed_url":   "https://www.youtube.com/feeds/videos.xml?playlist_id=PLdGl5mi0XeW3myi_yJGE89H8wPV-jJYPK",
+  "channel_id": "",
+},
