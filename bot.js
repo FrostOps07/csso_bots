6f0782ea2c05ae5928637846914e002597285f7d
Jon Glover <juandglover@gmail.com>
2018-12-06 12:46:56 -0800

    Clean up code for legibility, set permissions to true in dev environment

--- bot.js
+++ bot.js
@@ -8,6 +8,8 @@ const client  = new Discord.Client();
 const feed = require('./feedparser.js'); // For parsing RSS feeds
 const db   = require('./db.js');         // Database files
 
+const environment = process.env.NODE_ENV || 'development';
+
 // Read bot credentials from a file and log in
 let credentials = JSON.parse(fs.readFileSync('./db/client_key.json',"utf8"));
 client.login(credentials.token);
@@ -17,24 +19,22 @@ client.login(credentials.token);
 *   Creates an instance of the Discord bot. Should only be called on app startup.
 */
 exports.initBot = () => {
-
   console.log("Bot initializing.".yellow);
-
-  client.on("ready", () => {
-    console.log("Bot ready!");
-  });
-
+  client.on("ready",   ()        => { console.log("Bot ready!".green) });
   client.on("message", (message) => {
     // Message is a command
-    if( message.content.startsWith("!")){
+    if(message.content.startsWith("!")){
       // Data we will need to reference
       var msg_array  = message.content.split(" ");
       var channel_id = message.channel.id;
       var command    = msg_array[0].toLowerCase();
       var user_roles = [];
+
       // Check for roles with access to this command
       var roles_with_permission = db.rolesWithPermission(command);
       var has_permission = false;
+      if(environment == 'development') has_permission = true;
+      
       // Check if this user has a role with permission
       for (var i = 0; i < roles_with_permission.length; i++) {
         if(message.member.roles.has(roles_with_permission[i].id)){
@@ -42,131 +42,132 @@ exports.initBot = () => {
           user_roles.push(roles_with_permission[i].id);
         }
       }
+
       // Client has permission! Do the thing they asked.
       if(has_permission == true){
-        if ( command == "!commands"){
-          var command_list = db.getCommands(user_roles);
-          var command_list_message = "*Here are the commands you are able to use:* \n\n";
-          for (var i = 0; i < command_list.length; i++) {
-            command_list_message += "`"+command_list[i].command+"`: "+command_list[i].description+"\n\n";
-          }
-          message.channel.send(command_list_message);
-        }
-        else if ( command == "!test") {
-          message.channel.send("Reply!");
-        }
-        else if ( command == "!dyk") {
-          // Send DYK statistics
-          if(msg_array[1] == "stats"){
-            var dyk_stats_message = "<:dyk:324633372217573377> **BUT DID YOU KNOW???** <:dyk:324633372217573377>\n\n";
-            var dyk_stats = db.getDykStats();
+        switch (command) {
+          case "!commands":
+            let command_list         = db.getCommands(user_roles);
+            let command_list_message = "*Here are the commands you are able to use:* \n\n";
+            for (let c of command_list) {
+              command_list_message += `\`${c.command}\`: ${c.description}\n\n`;
+            }
+            message.channel.send(command_list_message);
+            break;
+          case "!test":
+            message.channel.send("Reply!");
+            break;
+          case "!dyk":
+            // Send DYK statistics
+            if(msg_array[1] == "stats"){ // !dyk stats
+              var dyk_stats_message = "<:dyk:324633372217573377> **BUT DID YOU KNOW???** <:dyk:324633372217573377>\n\n";
+              var dyk_stats         = db.getDykStats();
 
-            dyk_stats_message += "In this history of *Movies with Mikey*, we have learned **"+dyk_stats.total_facts+"** <:dyk:324633372217573377> facts. ";
-            dyk_stats_message += "<:dyk:324633372217573377> has been featured in **" + dyk_stats.episodes_with_facts + "** episodes so far, ";
+              dyk_stats_message += "In this history of *Movies with Mikey*, we have learned **"+dyk_stats.total_facts+"** <:dyk:324633372217573377> facts. ";
+              dyk_stats_message += "<:dyk:324633372217573377> has been featured in **" + dyk_stats.episodes_with_facts + "** episodes so far, ";
 
-            if(dyk_stats.most_facts.length > 1){
+              if(dyk_stats.most_facts.length > 1) {
+                dyk_stats_message += "and the episodes with the most <:dyk:324633372217573377>'s are ";
+                let factsIndex = 0;
+                for (let fact of dyk_stats.most_facts) {
+                   // If this is the last item, include 'and'
+                  if(factsIndex == (dyk_stats.most_facts.length - 1) ){ dyk_stats_message += `and *${fact.title}*`; }
+                  // Don't include a comma if there are only two items
+                  else if(dyk_stats.most_facts.length == 2){  dyk_stats_message += `*${fact.title}* `; }
+                  // If this item is not the last, include a comma
+                  else{ dyk_stats_message += `*${fact.title}*, `; }
+                  factsIndex++;
+                }
+                dyk_stats_message += ", with **" + dyk_stats.most_facts[0].fact_count + " <:dyk:324633372217573377>'s** each.";
+              }
+              else if(dyk_stats.most_facts.length == 1){
+                dyk_stats_message += `and the episode with the most <:dyk:324633372217573377>'s is *${dyk_stats.most_facts[0].title}*, with  **${dyk_stats.most_facts[0].fact_count} <:dyk:324633372217573377>'s**.`;
+              }
+              message.channel.send(dyk_stats_message);
+            }
+            // Send a random DYK fact
+            else {
+              // Grab a random DYK fact
+              var random_dyk    = db.getDYK();
 
-              dyk_stats_message += "and the episodes with the most <:dyk:324633372217573377>'s are ";
+              // If there's a prefix, I.E. "BONUS DID YOU KNOW???", add that here
+              var dyk_prefix    = "";
+              if(random_dyk.dyk_type != undefined){
+                dyk_prefix = random_dyk.dyk_type;
+              }
 
-              for (var i = 0; i < dyk_stats.most_facts.length; i++) {
-                if(i == (dyk_stats.most_facts.length - 1) ){ // if this is the last item
-                  dyk_stats_message += "and *"+dyk_stats.most_facts[i].title+"*";
-                } else if(dyk_stats.most_facts.length == 2){ // Don't include a comma if there are only two items
-                  dyk_stats_message += "*"+dyk_stats.most_facts[i].title+"* ";
-                }else{
-                  dyk_stats_message += "*"+dyk_stats.most_facts[i].title+"*, ";
+              var dyk_content   = random_dyk.dyk;
+              var dyk_link      = `<${random_dyk.videoLink}&t=${random_dyk.timestamp}>`;
+              var dyk_name      = random_dyk.title;
+
+              // Build the DYK message
+              var dyk_message = `<:dyk:324633372217573377> **${dyk_prefix} BUT DID YOU KNOW???** <:dyk:324633372217573377>  \n\n${dyk_content} \n\nFrom **${dyk_name}:** \n${dyk_link}`;
+
+              // Send that shit
+              message.channel.send(dyk_message);
+            }
+            break;
+          case "!new":
+            // Get the channel id the request was sent from
+            var feed_urls   = [];
+            // Look through all feeds
+            for (var i = 0; i < db.table.feeds.length; i++) {
+              // In each feed, look for all channel ids
+              for (var j = 0; j < db.table.feeds[i].channel_ids.length; j++) {
+                // If the channel the reqest was sent from matches the id for this feed, add it!
+                if(channel_id == db.table.feeds[i].channel_ids[j]){
+                  feed_urls.push(db.table.feeds[i].feed_url);
                 }
               }
-
-              dyk_stats_message += ", with **" + dyk_stats.most_facts[0].fact_count + " <:dyk:324633372217573377>'s** each.";
-
-            } else if(dyk_stats.most_facts.length == 1){
-              dyk_stats_message += "and the episode with the most <:dyk:324633372217573377>'s is *" + dyk_stats.most_facts[0].title + "*, with **" + dyk_stats.most_facts[0].fact_count + " <:dyk:324633372217573377>'s**.";
             }
-            message.channel.send(dyk_stats_message);
-          }
-          // Send a random DYK fact
-          else {
-            // Grab a random DYK fact
-            var random_dyk    = db.getDYK();
-            var dyk_prefix    = "";
-            if(random_dyk.dyk_type != undefined){
-              dyk_prefix = random_dyk.dyk_type;
-            }
-            var dyk_content   = random_dyk.dyk;
-            var dyk_link      = "<" + random_dyk.videoLink + "&t="+ random_dyk.timestamp + ">";
-            var dyk_name      = random_dyk.title;
-            // Build the DYK message
-            var dyk_message = "<:dyk:324633372217573377> **" + dyk_prefix + " BUT DID YOU KNOW???** <:dyk:324633372217573377> \n\n" + dyk_content + "\n\nFrom **" + dyk_name + ":** \n" + dyk_link;
-            // Send that shit
-            message.channel.send(dyk_message);
-          }
-        }
-        else if ( command == "!new") {
-          // Get the channel id the request was sent from
-          var feed_urls   = [];
-          // Look through all feeds
-          for (var i = 0; i < db.table.feeds.length; i++) {
-            // In each feed, look for all channel ids
-            for (var j = 0; j < db.table.feeds[i].channel_ids.length; j++) {
-              // If the channel the reqest was sent from matches the id for this feed, add it!
-              if(channel_id == db.table.feeds[i].channel_ids[j]){
-                feed_urls.push(db.table.feeds[i].feed_url);
-              }
-            }
-          }
-          // Remove any duplicate feed entries so we don't parse the same feeds multiple times
-          feed_urls = feed_urls.filter(function(item, pos) {
-            return feed_urls.indexOf(item) == pos;
-          });
-          // Send error if no feeds found, else get the newest item for each feed
-          if(feed_urls.length == 0){
-            message.channel.send("No feeds found for this channel."+ feed_urls);
-          }
-          // Don't bother comparing feeds if there's only one!
-          else if(feed_urls.length == 1){
-            feed.getNewest(feed_urls[0], function(result){
-              var episode_link = result.item.link;
-              var this_feed    = db.getFeedFromURL(feed_urls[0]);
-              var show_name    = this_feed.name;
-              var data = "Here's the newest episode of "+show_name+"! \n\n"+episode_link;
-              message.channel.send(data);
+            // Remove any duplicate feed entries so we don't parse the same feeds multiple times
+            feed_urls = feed_urls.filter(function(item, pos) {
+              return feed_urls.indexOf(item) == pos;
             });
-          }
-          // Channel is spamhalla, send the newest one from each feed
-          else if(channel_id == "eventually, spamhalla"){//"324587903759941632"){
-          }
-          // Otherwise, compare all the feeds!
-          else{
-            compareFeeds(feed_urls, function(data){
-              message.channel.send(data);
-            });
-          }
-        }
-        else {
-          // Only a user with permission for "all" would see this
-          // message.channel.send("The command `" + command + "` does not exist.");
+            // Send error if no feeds found, else get the newest item for each feed
+            if(feed_urls.length == 0){
+              message.channel.send("No feeds found for this channel."+ feed_urls);
+            }
+            // Don't bother comparing feeds if there's only one!
+            else if(feed_urls.length == 1){
+              feed.getNewest(feed_urls[0], function(result){
+                var episode_link = result.item.link;
+                var this_feed    = db.getFeedFromURL(feed_urls[0]);
+                var show_name    = this_feed.name;
+                var data = "Here's the newest episode of "+show_name+"! \n\n"+episode_link;
+                message.channel.send(data);
+              });
+            }
+            // Channel is spamhalla, send the newest one from each feed
+            else if(channel_id == "eventually, spamhalla"){//"324587903759941632"){
+            }
+            // Otherwise, compare all the feeds!
+            else{
+              compareFeeds(feed_urls, (data) => {
+                message.channel.send(data);
+              });
+            }
+            break;
         }
       }
       // Does not have permission and the command exists
       else if(has_permission == false && db.getCommand(command) != false){
         var error_msg  = "Hey you! You're not allowed to use `"+command+"`!\n\n";
             error_msg += "Roles with permission:\n";
-        for(var i = 0; i < roles_with_permission.length; i++){
-          error_msg += "- "+roles_with_permission[i].role+"\n";
+        for(let r of roles_with_permission){
+          error_msg += `- ${r.role}\n`;
         }
         message.channel.send(error_msg);
       }
     }
-
   });
 };
 
 /**
-*   @function compareFeeds
-*   Looks at multiple RSS feeds, and calls back with the newest episode
-*   @input feed_urls:array A list of feed URLs
-*/
+ *   @function    compareFeeds
+ *   @description Looks at multiple RSS feeds, and calls back with the newest episode
+ *   @input       feed_urls:array A list of feed URLs
+ */
 function compareFeeds(feed_urls, callback){
   console.log("Finding the newest episode within the following "+feed_urls.length+" feeds.");
   // Array of feed responses
@@ -186,16 +187,13 @@ function compareFeeds(feed_urls, callback){
         var this_feed    = db.getFeedFromURL(feed_data[0].feed_url);
         var episode_link = feed_data[0].item.link;
         var show_name    = this_feed.name;
-        // Feeds are the same show
+        // Feeds are the same show, include show name
         if(db.compareFeeds(feed_urls) == true){
-          // Create message
-          var data = "Here's the latest episode of "+show_name+"! \n\n"+episode_link;
-          callback(data);
+          callback(`Here's the latest episode of ${show_name}! \n\n${episode_link}`);
         }
         // Feeds are different shows, post CSSO text
         else{
-          var data = "Here's the latest release from FilmJoy!\n\n"+episode_link;
-          callback(data);
+          callback(`Here's the latest release from FilmJoy! \n\n${episode_link}`);
         }
       }
     });
@@ -208,9 +206,6 @@ function compareFeeds(feed_urls, callback){
 *   @input channel_id:string  The ID of the channel you want the message to be sent to
 */
 exports.sendMessage = (message, channel_id) => {
-  client.on("ready", () => {
-    // console.log(message, channel_id);
-    var channel = client.channels.get(channel_id);
-    channel.send(message);
-  });
+  var channel = client.channels.get(channel_id);
+      channel.send(message);
 };
--- db.js
+++ db.js
@@ -8,16 +8,16 @@ const dyk      = require("./db/dyk.json");
 const bot = require("./bot.js");
 
 exports.table = {
-  "commands":commands,
-  "feeds":feeds,
-  "roles":roles
+  "commands": commands,
+  "feeds":    feeds,
+  "roles":    roles
 }
 
 /**
-*   @function hasPermission()
-*   Check if user role has permission to use a command
-*   @return:boolean
-*/
+ *   @function    hasPermission()
+ *   @description Check if user role has permission to use a command
+ *   @return      hasPermission:boolean
+ */
 exports.hasPermission = (role_id, command) => {
   var role = exports.getRole(role_id);
   if(role != false){
@@ -33,47 +33,42 @@ exports.hasPermission = (role_id, command) => {
 }
 
 /**
-*   @function rolesWithPermission()
-*   Check what roles are able to use a command
-*   @return roles_with_permission:array Array of Role objects
-*/
+ *   @function    rolesWithPermission()
+ *   @description Check what roles are able to use a command
+ *   @return      roles_with_permission:array
+ */
 exports.rolesWithPermission = (command) => {
   var roles_with_permission = [];
-  // console.log("Checking for roles with permission to use "+command+".")
   // If a role has the desired command in their commands list,
   // Add them to the list of roles with permission to use this command
   for (var i = 0; i < roles.length; i++) {
     for (var j = 0; j < roles[i].commands.length; j++) {
       if(roles[i].commands[j] == command || roles[i].commands[j] == "all"){
         roles_with_permission.push(roles[i]);
-        // console.log("Adding role "+roles[i].role);
       }
     }
   }
-  // console.log("These roles have permission: "+roles_with_permission);
   return roles_with_permission;
 }
 
 /**
-*   @function getRole()
-*   Get a single role by ID
-*   @return role:object
-*/
+ *   @function    getRole()
+ *   @description Get a single role by ID
+ *   @return      role:object
+ */
 exports.getRole = (role_id) => {
-  for (var i = 0; i < roles.length; i++) {
-    if(role_id == roles[i].id){
-      return roles[i];
-    }
+  for (let r of roles) {
+    if(role_id == r.id) return r;
   }
   console.error("Searched for the role "+role_id+" but none was found.");
   return false;
 }
 
 /**
-*   @function getCommand()
-*   Get a single command by name
-*   @return command:object
-*/
+ *   @function    getCommand()
+ *   @description Get a single command by name
+ *   @return      command:object
+ */
 exports.getCommand = (command_name) => {
   for (var i = 0; i < commands.length; i++) {
     if(command_name == commands[i].command){
@@ -86,19 +81,19 @@ exports.getCommand = (command_name) => {
 
 /**
 *   @function getCommands()
-*   @input role_ids:array An array of role IDs
-*   @return command_list:array An array command objects
+*   @input    role_ids:array An array of role IDs
+*   @return   command_list:array An array command objects
 */
 exports.getCommands = (role_ids) => {
   var these_roles = [];
   // Get all roles requested
-  for (var i = 0; i < role_ids.length; i++) {
-    these_roles.push(exports.getRole(role_ids[i]));
+  for (let r of role_ids) {
+    these_roles.push(exports.getRole(r));
   }
   var command_list = [];
   // Get all commands these roles have
   these_roles.forEach(function(this_role){
-    if(this_role !=false && this_role.commands.length > 0){
+    if(this_role != false && this_role.commands.length > 0){
       for (var i = 0; i < this_role.commands.length; i++) {
         if(exports.hasPermission(this_role.id, this_role.commands[i])){
           // Just add all commands
@@ -123,10 +118,10 @@ exports.getCommands = (role_ids) => {
 }
 
 /**
-*   @function getFeedFromURL()
-*   @input feed_url:string A feed URL
-*   @return feed_item:object
-*/
+ *   @function getFeedFromURL()
+ *   @input    feed_url:string A feed URL
+ *   @return   feed_item:object
+ */
 exports.getFeedFromURL = (feed_url) => {
   for (var i = 0; i < feeds.length; i++) {
     if(feed_url == feeds[i].feed_url){
@@ -138,11 +133,11 @@ exports.getFeedFromURL = (feed_url) => {
 }
 
 /**
-*   @function compareFeeds()
-*   @input feed_urls:array
-*   Determines if multiple feeds are for the same show or not
-*   @return boolean
-*/
+ *   @function    compareFeeds()
+ *   @description Determines if multiple feeds are for the same show or not
+ *   @input       feed_urls:array
+ *   @return      boolean
+ */
 exports.compareFeeds = (feed_urls) => {
   var compared_feeds = [];
   for (var i = 0; i < feed_urls.length; i++) {
@@ -165,19 +160,18 @@ exports.compareFeeds = (feed_urls) => {
 }
 
 /**
-*   @function getDYK()
-*   Returns a random "But Did You Know??" fact from Movies with Mikey.
-*   @return dyk_fact:object
+*   @function    getDYK()
+*   @description Returns a random "But Did You Know??" fact from Movies with Mikey.
+*   @return      dyk_fact:object
 */
 exports.getDYK = () => {
-  var randomDYK = _.sample(dyk);
-  return randomDYK;
+  return _.sample(dyk);
 }
 
 /**
-*   @function getDykStats()
-*   Returns fun statistics about our DYK database.
-*   @return dyk_stats:object
+*   @function    getDykStats()
+*   @description Returns fun statistics about our DYK database.
+*   @return      dyk_stats:object
 */
 exports.getDykStats = () => {
   var this_dyk = dyk.slice();
